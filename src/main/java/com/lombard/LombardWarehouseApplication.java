c436099ca737cd031e0bf0102f1e71eca1d9e27e
Mr Torfis <sasapobizinskij@gmail.com>
2024-05-09 16:53:21 +0300

    [XXX] Bom update. Add docker img for bd. Add application.yml

--- src/main/java/com/lombard/LombardWarehouseApplication.java
+++ src/main/java/com/lombard/LombardWarehouseApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LombardApplication {
+public class LombardWarehouseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LombardApplication.class);
+        SpringApplication.run(LombardWarehouseApplication.class);
     }
 }
